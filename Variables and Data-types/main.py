55167c1bc68a460907844f927194e90dd26de5d2
sangam <sangamvermarr10@gmail.com>
2023-02-26 22:49:02 +0530

    Day #4

---
+++ Variables and Data-types/main.py
@@ -0,0 +1,12 @@
+a = int("4")  # str convert into int
+b = 4  # integer data-types
+print(a - b)
+
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
