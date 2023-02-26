72222d45dd576deaf1c30f5a8e216fe0dca06064
sangam <sangamvermarr10@gmail.com>
2023-02-26 22:20:08 +0530

    Day #3

---
+++ comments-print_statement/main.py
@@ -0,0 +1,20 @@
+# Comments in Python
+# Comments is lines of code which doesn't executed within your scripts
+''' Types of Comments 
+ 1 : "Single Line Comments"
+ 2 : "Multi Line Comments"
+'''
+# **Escape Characters**
+print("Hello, I am a \"good\"")
+# \'	Single Quote
+# \\	Backslash
+print("Hello \nHey")
+# \n	New Line
+# \r	Carriage Return
+# \t	Tab
+# \b	Backspace
+# \f	Form Feed
+# \ooo	Octal value
+# \xhh	Hex value
+
+print(3, 4, 5, 6,  sep="-",  end="\nHello")
