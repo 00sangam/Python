690c8a50c5cb0942def1f2e6c6b701a247d19e3f
sangam <sangamvermarr10@gmail.com>
2023-02-26 21:12:45 +0530

    Day #1

---
+++ Modules-and-Pip/main.py
@@ -0,0 +1,6 @@
+# Modules is like a code library which can be used as borrow the code written by somebody else in our python program 
+# ******Types of Modules********  
+#1 : Built-in modules
+#2 : External modules
+
+#PIP is a pakeage maneger which is used to install pakeages 
